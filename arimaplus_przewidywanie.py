ece329e2e1bc64065bd5805f30e153dd81a6df19
Marcin <marcindahlen@gmail.com>
2017-10-03 14:47:29 +0200

    ARIMA is working now

--- arimaplus_przewidywanie.py
+++ arimaplus_przewidywanie.py
@@ -6,81 +6,89 @@ as base to calculating loss functions.
 import arimaplus_math as ap_math
 
 
-def licz_reg_liniowa(okno, wejscie=[]):
+def linearRegression(window, input=[]):
     """For a given list of numbers and a window frame,
     slides the frame over sublist of input data
-    and returns linear regression coeficient one - for each frame."""
-    baza = [n for n in range(0, int(okno))]
+    and returns linear regression coeficient - one for each frame."""
+    baza = [n for n in range(0, int(window))]
     output = []
-    for i in range(0, int(len(wejscie) / okno) - 1):
-        output.append(ap_math.regresja_liniowa(baza, wejscie[int(i * okno):int(i * okno + okno)]))
+    for i in range(0, int(len(input) / window) - 1):
+        output.append(ap_math.linearRegression(baza, input[int(i * window):int(i * window + window)]))
 
     return output
 
 
-def licz_reg_wielomianiowa(okno, n, wejscie=[]):
+def polynomialRegression(window, n, input=[]):
     """For a given list of numbers and a window frame,
     slides the frame over sublist of input data
     and returns polynomial regression coeficients for each frame."""
-    baza = [n for n in range(0, len(okno))]
+    baza = [n for n in range(0, len(window))]
     output = []
-    for i in range(0, len(wejscie / okno) - 1):
-        output.append(ap_math.regresja_wielomianowa(baza, wejscie[i * okno:i * okno + okno], n))
+    for i in range(0, len(input / window) - 1):
+        output.append(ap_math.polynomialRegression(baza, input[i * window:i * window + window], n))
 
     return output
 
-
-def roznicoj_dane(q, wejscie=[]):
+# TODO
+def DataDifferentiation(q, input=[]):
     """For a given list of numbers creates
     a corresponding list of differenced data,
     making data stationary (I in ARIMA)."""
-    output = wejscie
+    output = input
+    print("DataDifferentiation for q=" + str(q) + " input: ")
+    print(input)
     if q == 1:
         for i in range(1, len(output)):
-            output[i] = wejscie[i] - wejscie[i - 1]
+            output[i] = input[i] - input[i - 1]
     if q == 2:
+        output[0] = 0
+        output[1] = 0
         for i in range(2, len(output)):
-            output[i] = wejscie[i] - 2 * wejscie[i - 1] + wejscie[i - 2]
+            output[i] = input[i] - input[i - 1]
     if q == 3:
         for i in range(3, len(output)):
-            output[i] = wejscie[i] - 2 * wejscie[i - 1] + wejscie[i - 3]
+            output[i] = input[i] - input[i - 1]
 
-    output = ap_math.normalizuj(output)
+    output = ap_math.normalise(output)
 
     return output
 
-
-def interpretuj_roznicowanie(q, wejscie=[]):
+# TODO
+def interpretDifferentiation(q, wejscie=[]):
     """Inverts the differencing process."""
     pass
 
 
-def ARIMA(p, d, cA, cB, cC, eA, eB, eC, wejscie=[]):
+def ARIMA(p, d, cA, cB, cC, eA, eB, eC, input=[]):
     """Predicts data series values using ARIMA(p, q, d) model.
     Returns list of pairs: output value, error."""
-    rownanie = ""
+    formula = ""
     output = []
-    error = []
-    srednia = ap_math.srednia(wejscie)
+    error = [0, 0, 0]
+    average = ap_math.average(input)
 
-    rownanie += "srednia"
+    formula += "average"
     if p == 1:
-        rownanie += "+ cA*x"
+        formula += "+ cA*x"
     if p == 2:
-        rownanie += "+ cA*x+cB*y"
+        formula += "+ cA*x+cB*y"
     if p == 3:
-        rownanie += "+ cA*x+cB*y+cC*z"
+        formula += "+ cA*x+cB*y+cC*z"
     if d == 1:
-        rownanie += "-eA*m"
+        formula += "-eA*m"
     if d == 2:
-        rownanie += "-eA*m -eB*n"
+        formula += "-eA*m -eB*n"
     if d == 3:
-        rownanie += "-eA*m -eB*n -eC*r"
+        formula += "-eA*m -eB*n -eC*r"
 
-    for i in range(0, len(wejscie)):
-        x = wejscie[i - 1]
-        y = wejscie[i - 2]
-        z = wejscie[i - 3]
+    print("ARIMA(" + str(p) + ", " + str(d) + ") formula for (" + " " + str(cA) + " " + str(cB) + " " + str(cC) + " " + str(eA) + " " + str(eB) + " " + str(eC) + "): " + formula)
+    print("ARIMA(" + str(p) + ", " + str(d) + ") input data with average=" + str(average) + ": ")
+    print(input)
+
+    for i in range(3, len(input)):
+        x = input[i - 1]
+        y = input[i - 2]
+        z = input[i - 3]
         m = 0
         n = 0
         p = 0
@@ -90,7 +98,7 @@ def ARIMA(p, d, cA, cB, cC, eA, eB, eC, wejscie=[]):
             n = error[i - 2]
         elif i >= 3:
             r = error[i - 3]
-        output.append(eval(rownanie))
-        if i < len(wejscie):
-            error.append(wejscie[i] - eval(rownanie))
+        output.append(eval(formula))
+        if i < len(input):
+            error.append(input[i] - eval(formula))
     return zip(output, error)
