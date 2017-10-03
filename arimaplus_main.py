ca5c5f89d8a275bc9fb222d89838699d71c80046
Marcin <marcindahlen@gmail.com>
2017-10-03 14:45:32 +0200

    Update naming

--- arimaplus_main.py
+++ arimaplus_main.py
@@ -1,26 +1,28 @@
 import  arimaplus_dna as dna
 import os.path
 
-#Read input data
-plik = os.path.expanduser("~/Documents/dane.csv")
-with open(plik) as f:
-    dane = f.readlines()
-dane = [x.strip() for x in dane]
-dane = [z.replace(',', '.') for z in dane]
-dane = list(map(float, dane))
+# Read input data
+theFile = os.path.expanduser("~/Documents/dane.csv")
+with open(theFile) as f:
+    data = f.readlines()
+data = [x.strip() for x in data]
+data = [z.replace(',', '.') for z in data]
+data = list(map(float, data))
+print("Original data of length " + str(len(data)) + ":")
+print(data)
 
-#check for file with DNA code - if not find, create population and evaluate until winner organism emerges
+# check for file with DNA code - if not find, create population and evaluate until winner organism emerges
 if os.path.isfile(os.path.expanduser("~/Documents/the_dna.txt")):
     with open(os.path.expanduser("~/Documents/the_dna.txt")) as f:
-        to_dna = f.readline()
-    jednostka = dna.jednostka(to_dna)
-    jednostka.przewidywanie(dane)
-    #jednostka.rysowanie(8, jednostka.dane_out)
+        this_dna = f.readline()
+    entity = dna.entity(this_dna)
+    entity.forecasting(data)
+    #entity.rysowanie(8, jednostka.dane_out)
 else:
-    pop = dna.populacja()
-    pop.ewaluacja(dane)
-    while (not pop.anageneza(dane)):
-        pop.ewaluacja(dane)
+    pop = dna.population()
+    pop.evaluation(data)
+    while (not pop.anagenesis(data)):
+        pop.evaluation(data)
     #for el in pop.pokolenie:
     #    el.rysowanie(8, el.dane_out)
 
